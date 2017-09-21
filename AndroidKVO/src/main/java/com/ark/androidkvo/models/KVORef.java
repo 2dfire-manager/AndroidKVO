b4f2b96c16b2ef250eb9ac1a433e1f54371f9d8e
dove <flyer88xu@gmail.com>
2017-09-21 13:24:48 +0800

    删除无用代码

--- AndroidKVO/src/main/java/com/ark/androidkvo/models/KVORef.java
+++
@@ -1,9 +0,0 @@
-//package com.ark.androidkvo.models;
-//
-///**
-// * Created by dove on 2017/8/28.
-// */
-//
-//public interface KVORef<T extends IKVO> extends IKVO<T>{
-//    public KVOObserverObject getObserverObject(String field);
-//}
